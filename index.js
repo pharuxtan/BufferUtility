14d37e0571888b326502f3a5ad9c1b42ce6a9655
Pharuxtan <alex360noscop@gmail.com>
2020-08-15 00:54:44 +0200

    auto create file

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-module.exports = {
-  BufferUtility: require("./lib/BufferUtility"),
-  v1: {
-    BufferReader: require("./lib/BufferReader"),
-    BinaryReader: require("./lib/BufferReader"),
-    BufferWriter: require("./lib/BufferWriter"),
-    BinaryWriter: require("./lib/BufferWriter")
-  }
+console.time(0);
+arr = [1, 2, 3];
+for(let i in arr){
+  arr[i] = arr[i]*2
 }
+console.timeLog(0);
+console.time(1);
+arr = [1, 2, 3];
+arr.forEach((value, index) => {
+  arr[index] = value*2;
+});
+console.timeLog(1);
+console.time(2);
+arr = [1, 2, 3];
+arr = arr.map((value) => {
+  return value*2;
+});
+console.timeLog(2);
